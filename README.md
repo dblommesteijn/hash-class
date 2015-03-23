654617ba84c6a7095483dfc410c3644fd7bddf2d
Dennis Blommesteijn <dennis@blommesteijn.com>
2015-03-23 11:29:20 +0100

    Add specs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# hash-class
+Hash-class will extend the Hash object with `assign_if`. This function reduces the verbosity when conditionaly assigning values from element within the basic Hash object.
+
+Like: `a = hash[:some_element] if hash.include?(:some_element)`.
+Replaced by: `hash.assign_if(:some_element) {|e| a = e}`
+
+
+
+
+
--- lib/hash-class/assign_if.rb
+++ lib/hash-class/assign_if.rb
@@ -2,24 +2,14 @@ module HashClass
   module AssignIf
 
     # lookup element and return value if exists
-    def assign_if(&block)
-      # puts options
-      block.call(self)
-      # if not options.is_a?(Hash)
-      #   el = options.first
-      #   block.call(self[el]) if self.include?(el)
-      # end
+    def assign_if(*opts, &block)
+      if opts.is_a?(Array)
+        el = opts.first
+        if block.is_a?(Proc)
+          return block.call(self[el]) if self.include?(el)
+        end
+      end
     end
-
-    def method_missing(m, *args, &block)
-      self[m]
-      # puts args
-      # puts block
-    end
-
-    # def a(*opts)
-    #   puts self
-    # end
 
   end
 end
